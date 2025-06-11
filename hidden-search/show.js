5b8c82703686a8c79d2b5e3025eae71811842618
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-06-11 12:30:32 +0800

    Changed layout and style
    Changed the overall vibe to be a bit more modern, also added a clicking sound on button press.

--- hidden-search/show.js
+++ hidden-search/show.js
@@ -1,13 +1,21 @@
 const search_bar = document.getElementById("search-bar");
 const search_button = document.getElementById("btn");
+const search_input = document.getElementById("input");
+const soundPop = new Audio((src = "pop.mp3"));
+
 let searchState = 0;
 
 search_button.addEventListener("click", () => {
+  soundPop.currentTime = 0; // Reset sound to start
+  soundPop.play();
   if (searchState === 0) {
     searchState++;
     search_bar.classList.remove("hidden");
+    search_button.style.borderRadius = "0px 6px 6px 0px";
+    search_input.focus();
   } else {
     searchState--;
     search_bar.classList.add("hidden");
+    search_button.style.borderRadius = "6px";
   }
 });
