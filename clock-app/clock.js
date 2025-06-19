d126e3dcfde3f59ccaf59c722435f5f15b4d99f7
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-06-19 17:21:33 +0800

    Added clock app to repo

---
+++ clock-app/clock.js
@@ -0,0 +1,37 @@
+// Analog Clock Elements
+const hourHand = document.querySelector("#hour-hand");
+const minHand = document.querySelector("#min-hand");
+const secHand = document.querySelector("#sec-hand");
+// Digital Clock Elemets
+const hourDig = document.getElementById("hour-time");
+const minDig = document.getElementById("min-time");
+const secDig = document.getElementById("sec-time");
+
+const meridiem = document.getElementById("meridiem");
+function updateClock() {
+  let time = new Date();
+
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+
+  hourDig.textContent = hour;
+  minDig.textContent = min;
+  secDig.textContent = sec;
+
+  if (hour >= 12) {
+    meridiem.textContent = " " + "PM";
+  } else {
+    meridiem.textContent = " " + "AM";
+  }
+
+  const hrDeg = ((hour % 12) / 12) * 360 + (min / 60) * 30;
+  const minDeg = (min / 60) * 360 + (sec / 60) * 6;
+  const secDeg = (sec / 60) * 360;
+
+  hourHand.style.transform = `translate(-50%, -100%) rotate(${hrDeg}deg)`;
+  minHand.style.transform = `translate(-50%, -100%) rotate(${minDeg}deg)`;
+  secHand.style.transform = `translate(-50%, -100%) rotate(${secDeg}deg)`;
+}
+
+setInterval(updateClock, 1000);
