00e913963f259cd1a00c5ed1741e6476c0a43f1b
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-06-29 23:13:05 +0800

    Add to repo

---
+++ todo-list/list.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const list = document.getElementById("form");
+
+let todo = JSON.parse(localStorage.getItem(todos));
+
+function addTodoList() {}
+function addToLocalStorage() {}
