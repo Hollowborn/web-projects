6ec2cd3409d4e06c02dfe0f66b692e962914936e
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-06-30 18:42:23 +0800

    added a working delete tasks via mouse right click.

--- todo-list/list.js
+++ todo-list/list.js
@@ -1,7 +1,5 @@
 const form = document.getElementById("form");
 const inputValue = document.getElementById("input");
-
-// let todo = JSON.parse(localStorage.getItem(todos));
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
 let ulState = 0;
 if (todos) {
@@ -11,14 +9,14 @@ if (todos) {
 }
 
 // Create on click listener for form submit
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   addTodoList(inputValue.value);
-  addToLocalStorage(inputValue.value);
+  addToLocalStorage();
   inputValue.value = "";
 });
+
+// Event listener for removing
 
 function addTodoList(task) {
   if (ulState == 0) {
@@ -29,9 +27,20 @@ function addTodoList(task) {
   }
   const li = document.createElement("li");
   li.appendChild(document.createTextNode(task));
+  li.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    li.remove();
+    addToLocalStorage(); // Update storage after removal
+  });
   todo_list.appendChild(li);
+  addToLocalStorage();
 }
-function addToLocalStorage(task) {
-  todos.push(task);
-  localStorage.setItem("todos", JSON.stringify(todos));
+
+function addToLocalStorage() {
+  let todoElements = document.querySelectorAll("li");
+  const todosHere = [];
+  todoElements.forEach((element) => {
+    todosHere.push(element.textContent);
+  });
+  localStorage.setItem("todos", JSON.stringify(todosHere));
 }
