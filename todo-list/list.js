c56c7ca1f5a5d942fb47c9fb925a326f3b4072cc
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-06-30 17:39:21 +0800

    added functional functions for storing and retrieving todo data from local sotrage

--- todo-list/list.js
+++ todo-list/list.js
@@ -1,8 +1,37 @@
 const form = document.getElementById("form");
-const input = document.getElementById("input");
-const list = document.getElementById("form");
+const inputValue = document.getElementById("input");
 
-let todo = JSON.parse(localStorage.getItem(todos));
+// let todo = JSON.parse(localStorage.getItem(todos));
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+let ulState = 0;
+if (todos) {
+  todos.forEach((todo) => {
+    addTodoList(todo);
+  });
+}
 
-function addTodoList() {}
-function addToLocalStorage() {}
+// Create on click listener for form submit
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addTodoList(inputValue.value);
+  addToLocalStorage(inputValue.value);
+  inputValue.value = "";
+});
+
+function addTodoList(task) {
+  if (ulState == 0) {
+    const todo_list = document.createElement("ul");
+    todo_list.setAttribute("id", "todo_list");
+    form.appendChild(todo_list);
+    ulState++;
+  }
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(task));
+  todo_list.appendChild(li);
+}
+function addToLocalStorage(task) {
+  todos.push(task);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
