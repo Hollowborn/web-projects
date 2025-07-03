b52fe2dc007a41040e7372bd532ac3302a356e87
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-07-03 23:33:01 +0800

    added inital code

---
+++ github-profiles-search/api_search.js
@@ -0,0 +1,8 @@
+const urlAPI = `https://api.github.com/user`;
+
+const form = document.getElementById("form");
+const inputValue = document.getElementById("userSearch");
+
+form.addEventListener("submit", (e) => {
+  // Do stuff here
+});
