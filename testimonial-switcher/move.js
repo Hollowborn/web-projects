bdea4bab90689646e5b79250f6bad60e1ff6c07c
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-07-19 17:54:17 +0800

    added initial testimonies

---
+++ testimonial-switcher/move.js
@@ -0,0 +1,31 @@
+const testimonies = [
+  {
+    name: "Mike Wazoskiee",
+    title: "Monstah",
+    message: "This guy knows!!",
+  },
+  {
+    name: "John Cena",
+    title: "Professional WWE Wrestler",
+    message: "You can't see me. My time is now.",
+  },
+  {
+    name: "Tim Cook",
+    title: "Professional Cooker",
+    message:
+      "This guys cooks, btw. Hire him, make him rich. Do it so, and he will make you more rich.",
+  },
+  {
+    name: "Pikachu",
+    title: "pikapika",
+    message:
+      "pika pika, pikachu!! pikachu? p i k a c h u?? pika pika pika pika... CHUUUUUU!!!!!",
+  },
+  {
+    name: "Picori",
+    title: "ricopico",
+    message: "ripo pico picori, ripo rici? picori?",
+  },
+];
+
+const testimonyMessage = document.getElementById("msg");
