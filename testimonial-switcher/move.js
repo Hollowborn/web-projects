da8b4b0cff7d654925a75ac54188f3d77376a4f7
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-07-20 22:56:23 +0800

    modified image file path

--- testimonial-switcher/move.js
+++ testimonial-switcher/move.js
@@ -3,29 +3,37 @@ const testimonies = [
     name: "Mike Wazoskiee",
     title: "Monstah",
     message: "This guy knows!!",
+    src: "./images/mike.jpg",
   },
   {
     name: "John Cena",
     title: "Professional WWE Wrestler",
     message: "You can't see me. My time is now.",
+    src: "./images/cena.jpg",
   },
   {
     name: "Tim Cook",
     title: "Professional Cooker",
     message:
       "This guys cooks, btw. Hire him, make him rich. Do it so, and he will make you more rich.",
+    src: "./images/pikachu.jpg",
   },
   {
     name: "Pikachu",
     title: "pikapika",
     message:
       "pika pika, pikachu!! pikachu? p i k a c h u?? pika pika pika pika... CHUUUUUU!!!!!",
+    src: "./images/pikachu.jpg",
   },
   {
     name: "Picori",
     title: "ricopico",
     message: "ripo pico picori, ripo rici? picori?",
+    src: "./images/picori.jpg",
   },
 ];
 
 const testimonyMessage = document.getElementById("msg");
+const testimonyImage = document.getElementById("img");
+const testimonyTitle = document.getElementById("title");
+const testimonyName = document.getElementById("name");
