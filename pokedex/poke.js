b47610a4952a9948c3899efc927fddf34b64eb4c
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-08-05 23:55:30 +0800

    added some base js code

--- pokedex/poke.js
+++ pokedex/poke.js
@@ -1,1 +1,36 @@
-const types = {};
+const container = document.getElementById("container");
+const count = 100;
+const types = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
+
+
+function getPokemons(){
+    for(i = 1; i <= count; i++){
+        fetchPokemons(i);
+    }
+}
+
+function fetchPokemons(index){
+    const url = `https://pokeapi.co/api/v2/pokemon/${index}`;
+    const result = await fetch(url);
+    const data = await result.json();
+    generateCard(data);
+}
+
+function generateCard(pokemon){
+
+}
